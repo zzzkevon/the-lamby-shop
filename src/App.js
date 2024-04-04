db75bfb1b3ed7e4100090c251de3dcd42a26199f
StevenH01 <105683557+StevenH01@users.noreply.github.com>
2024-04-04 12:54:19 -0700

    created navbar and adjusted page routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
 import NavBar from './components/NavBar';
 import HeroSection from './components/HeroSection';
 import AboutSection from './components/AboutSection';
-import Footer from './components/Footer';
+// import Footer from './components/Footer';
 
 function App() {
+  let screen
+  switch (window.location.pathname) {
+    case "/":
+      screen = <HeroSection />
+      break;
+    case "/about":
+      screen = <AboutSection />
+      break;
+    // case "/":
+    //   screen = <HeroSection />
+    //   break;
+    // case "/":
+    //   screen = <HeroSection />
+    //   break;
+    // case "/":
+    //   screen = <HeroSection />
+    //   break;
+    // case "/":
+    //   screen = <HeroSection />
+    //   break;
+    default:
+      break;
+  }
   return (
-    <div>
+    <>
       <NavBar />
-      <HeroSection />
-      <AboutSection />
-      <Footer />
-    </div>
+      {screen}
+    </>
   );
 }
 
