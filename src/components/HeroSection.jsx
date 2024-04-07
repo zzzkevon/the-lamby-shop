93a47644fd07e08856b0425a3c6dd0f2792e8567
longlivejon <jono_nat@yahoo.com>
2024-04-07 01:56:40 -0700

    added the second part of carousel

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Subscribe from './subscribe/Subscribe';
+import Carousel2 from './carousel2/Carousel2'
 const HeroSection = () => {
   return (
     <div>
       HeroSection
+      <Carousel2 />
       <Subscribe />
-
     </div>
 
   );
