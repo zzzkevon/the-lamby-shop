cf1c4efcd604205769f813ce8ad594f4cc78caef
Alex4 <Alex4souv2@gmail.com>
2024-04-15 21:45:15 -0700

    DT-14 Added a link to ProfileSection to route to Create Account page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ProfileSection from './components/ProfileSection';
 import ShopSection from './components/ShopSection';
 import CommisionsSection from './components/CommissionsSection';
 import ContactSection from './components/ContactSection';
+import CreateAccount from './components/CreateAccount';
 // import Footer from './components/Footer';
 
 function App() {
@@ -31,6 +32,9 @@ function App() {
       break;
     case "/shoppingcart":
       screen = <ShoppingCart />
+      break;
+    case "/createaccount":
+      screen = <CreateAccount />
       break;
     default:
       break;
