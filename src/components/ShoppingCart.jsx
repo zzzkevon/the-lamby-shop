1f9118d73a86f4664cb4e69873cb44939197895c
kevinwong2 <kevinwong4@csus.edu>
2024-09-15 16:28:27 -0700

    Connected AWS inventoryAPI to inventory page + added a prototype cart

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { addToCart, getCart, removeFromCart, setCart } from './cart/cart.jsx';
 
 const ShoppingCart = () => {
+  console.log("cart=", getCart());
   return (
     <div>Shopping Cart</div>
   );
