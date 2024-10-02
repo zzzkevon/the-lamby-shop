2c27c97d5fabc9b1fcaea5e9f82dfb83101ff9b2
dannylphan <danphan916@gmail.com>
2024-10-01 23:00:14 -0700

    added update function to manage inventory

--- src/components/adminPages/AdminDashboard.jsx
+++ src/components/adminPages/AdminDashboard.jsx
@@ -1,11 +1,18 @@
 import star from '../../images/story_stars_1.png';
 import React from 'react';
-import { useGetAll } from '../inventoryAPI/functionCalls';
+import { addItem } from '../inventoryAPI/functionCalls';
 import ManageInventory from '../inventory/ManageInventory';
 
 const AdminDashboard = () => {
     // const items = useGetAll()
     const [showInventoryModal, setShowInventoryModal] = React.useState(false);
+    const [showAddModal, setShowAddModal] = React.useState(false);
+
+    const [itemName, setItemName] = React.useState('');
+    const [imageKey, setImageKey] = React.useState('');
+    const [itemDescription, setItemDescription] = React.useState('');
+    const [itemPrice, setItemPrice] = React.useState('');
+    
 
     const useManageInventory = () => {
         console.log("manage inventory clicked.");
@@ -19,6 +26,21 @@ const AdminDashboard = () => {
     const closeModal = () => {
         setShowInventoryModal(false); // Hide the modal when called
     };
+
+    const createPayload = () => {
+        console.log("add product clicked.");
+        const payload = {
+            body: {
+                itemName: itemName,
+                imageKey: imageKey,
+                itemDescription: itemDescription,
+                itemPrice: itemPrice
+            }
+        };
+        addItem(payload);
+        // console.log("Payload: ", JSON.stringify(payload));
+        setShowAddModal(false);
+    }
 
     return (
         <div className="main-bg just-another-hand 4xl">
@@ -59,15 +81,71 @@ const AdminDashboard = () => {
 
                             <div className="flex flex-row w-full md:w-1/2 mt-8 m-10 space-x-10">
 
-                                {/* <div className="flex flex-col items-center mt-2 ">  */}
-                                    {/* button should call procedure to manage products*/}
-                                    {/* <button className="bg-[#780000] hover:bg-[#780000] text-2xl text-white py-2 px-10 rounded-full mt-6 mr-8">
-                                        Add/Delete Products
+                                <div className="flex flex-col items-center mt-2 ">
+                                    {/* button should call procedure to add products*/}
+                                    <button className="bg-[#780000] hover:bg-[#780000] text-2xl text-white py-2 px-10 rounded-full mt-6 mr-8"
+                                    onClick={() => setShowAddModal(true)}>
+                                        Add Product
                                     </button>
-                                    
-                                </div> */}
+                                </div>
+                                {showAddModal && (
+                                    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+                                        <div className="bg-white p-6 rounded-lg w-1/3">
+                                            <h2 className="text-xl mb-4">Add New Item</h2>
+                                            <div className="mb-4">
+                                                <label className="block mb-1">Item Name:</label>
+                                                <input
+                                                    type="text"
+                                                    className="w-full p-2 border rounded"
+                                                    value={itemName}
+                                                    onChange={(e) => setItemName(e.target.value)}
+                                                />
+                                            </div>
+                                            <div className="mb-4">
+                                                <label className="block mb-1">Image Key:</label>
+                                                <input
+                                                    type="text"
+                                                    className="w-full p-2 border rounded"
+                                                    value={imageKey}
+                                                    onChange={(e) => setImageKey(e.target.value)}
+                                                />
+                                            </div>
+                                            <div className="mb-4">
+                                                <label className="block mb-1">Item Description:</label>
+                                                <input
+                                                    type="text"
+                                                    className="w-full p-2 border rounded"
+                                                    value={itemDescription}
+                                                    onChange={(e) => setItemDescription(e.target.value)}
+                                                />
+                                            </div>
+                                            <div className="mb-4">
+                                                <label className="block mb-1">Item Price:</label>
+                                                <input
+                                                    type="text"
+                                                    className="w-full p-2 border rounded"
+                                                    value={itemPrice}
+                                                    onChange={(e) => setItemPrice(e.target.value)}
+                                                />
+                                            </div>
+                                            <div className="flex justify-end">
+                                                <button
+                                                    className="bg-gray-500 hover:bg-gray-700 text-white py-2 px-4 rounded mr-2"
+                                                    onClick={() => setShowAddModal(false)}
+                                                >
+                                                    Cancel
+                                                </button>
+                                                <button
+                                                    className="bg-[#780000] hover:bg-[#8B0000] text-white py-2 px-4 rounded"
+                                                    onClick={createPayload}
+                                                >
+                                                    Add Item
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )}
                                 {/*manage product's category, price, etc.*/}
-
                                 <button className="bg-[#780000] hover:bg-[#780000] text-2xl text-white py-2 px-10 rounded-full mt-6 mr-8"
                                 onClick={useManageInventory}>
                                 Manage Inventory
@@ -80,12 +158,6 @@ const AdminDashboard = () => {
                                         </div>
                                     </div>
                                 )}
-
-                                <a href="/admin/admin-manage-inventory">
-                                    <button className="bg-[#780000] hover:bg-[#780000] text-2xl text-white py-2 px-10 rounded-full mt-6 mr-8">
-                                    Manage Inventory
-                                        </button>
-                                </a>
                             </div>
                         </div>
         
