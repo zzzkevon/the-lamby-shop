069f142db43a27d75f8b975925223ea4a6652cb3
Alex4 <Alex4souv2@gmail.com>
2024-10-15 18:46:26 -0700

    Changed Return Home to be from a link reference to a button

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const NotFound = () => {
+    const navigate = useNavigate();
     return (
-        <div className="not-found-page just-another-hand text-center text-6xl mb-4">
+        <div className="flex flex-col text-4xl just-another-hand justify-center items-center min-w-screen min-h-screen -mt-32">
         <h1>404 - Page Not Found</h1>
         <p> Oops! The page you're looking for doesn't exist. </p>
-        <Link to="/">Return to Home</Link>
+        <ul className="flex flex-col items-center">
+                <li>
+                    <button className="bg-[#780000] hover:bg-[#780000] py-2 px-4 rounded-full text-white mt-4" onClick={() => navigate('/')}>Return home</button>
+                </li>
+                </ul>
         </div>
     );
 };
