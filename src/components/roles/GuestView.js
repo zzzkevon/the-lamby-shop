f51d956b60a6d49187b326bc18732ef469b82abe
Alex4 <Alex4souv2@gmail.com>
2024-10-15 18:28:11 -0700

    Modified the view pages to be dashboards for each role, and made the RoleBasedView display the correct dashboard based off the role selected in the top right dropdown menu

--- src/components/roles/GuestView.js
+++ src/components/roles/GuestView.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const GuestView = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>Guest Dashboard</h1>
+    <div className="flex flex-col text-4xl just-another-hand justify-center items-center min-w-screen min-h-screen -mt-32">
+      <h2 >Guest Dashboard</h2>
       <p>Welcome, Guest! You can browse but will have limited access.</p>
+      <ul className= "flex flex-col items-center">
+        <li>
+          <button className="bg-[#780000] hover:bg-[#780000] py-2 px-4 rounded-full text-white mt-4" onClick={() => navigate('/profile')}>Sign In</button>
+        </li>
+        <li>
+          <button className="bg-[#780000] hover:bg-[#780000] py-2 px-4 rounded-full text-white mt-4" onClick={() => navigate('/createaccount')}>Create Account</button>
+        </li>
+        </ul>
     </div>
   );
 };
