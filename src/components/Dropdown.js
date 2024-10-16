0dbcbc13c73721d0059fb0ee4f8f68fe11650d3d
Alex4 <Alex4souv2@gmail.com>
2024-10-15 18:31:09 -0700

    Added storage localization for the user roles to be stored and read

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -7,12 +7,12 @@ const Dropdown = ({ userRole, setUserRole }) => {
 
     const handleRoleChange = (role) => {
         setUserRole(role);
+        localStorage.setItem('userRole', role);
         setIsOpen(false); // Close the dropdown after selecting a role
     };
 
     return (
         <div className="relative inline-block text-right z-50">
-            {/* The button to toggle the dropdown */}
             <button onClick={toggleDropdown} className="main-bg p-1 rounded text-med">
                 {userRole.charAt(0).toUpperCase() + userRole.slice(1)} View
             </button>
@@ -21,22 +21,13 @@ const Dropdown = ({ userRole, setUserRole }) => {
             {isOpen && (
                 <div className="absolute right-0 bg-main rounded p-0 w-24">
                     <ul className="flex flex-col text-2xl">
-                        <li
-                            className="p-1 hover:bg-gray-100 cursor-pointer"
-                            onClick={() => handleRoleChange('guest')}
-                        >
+                        <li className="p-1 hover:bg-gray-100 cursor-pointer" onClick={() => handleRoleChange('guest')}>
                             Guest View
                         </li>
-                        <li
-                            className="p-1 hover:bg-gray-100 cursor-pointer"
-                            onClick={() => handleRoleChange('customer')}
-                        >
+                        <li className="p-1 hover:bg-gray-100 cursor-pointer" onClick={() => handleRoleChange('customer')}>
                             Customer View
                         </li>
-                        <li
-                            className="p-1 hover:bg-gray-100 cursor-pointer"
-                            onClick={() => handleRoleChange('admin')}
-                        >
+                        <li className="p-1 hover:bg-gray-100 cursor-pointer" onClick={() => handleRoleChange('admin')}>
                             Admin View
                         </li>
                     </ul>
