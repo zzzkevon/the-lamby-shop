a9f0029cc36c7152e9d86f7a9ed0e27125b64a27
Sam <47896048+Fake-Sam@users.noreply.github.com>
2024-12-04 19:07:34 -0800

    Commissions fix
    Fix to guest commissions page being shown when a different view should be shown. Page reloads after two seconds to show the correct view.

--- src/components/CommissionsSection/CommissionsSection.jsx
+++ src/components/CommissionsSection/CommissionsSection.jsx
@@ -4,9 +4,10 @@ import UserCommisionsSection from './UserCommissionsSection';
 import GuestCommissionSection from './GuestCommissionSection';
 
 export default function CommissionsSection({ userRole, email, username }) {
+  const role = localStorage.getItem('userRole');
     return (
       <div>
-        {userRole === "admin" ? (
+        {userRole === "admin" || role === 'admin'? (
           <AdminCommissionSection />
         ) : userRole === "customer" || userRole === "user" ? (
           <UserCommisionsSection userEmail={email} username={username}/>
--- src/components/profiles/CustomerProfile.js
+++ src/components/profiles/CustomerProfile.js
@@ -3,7 +3,8 @@ import { useNavigate } from 'react-router-dom';
 
 const CustomerProfile = ({ handleSignOut }) => {
     const navigate = useNavigate();
-
+    localStorage.setItem('hasReloaded', 'false');
+    localStorage.setItem("userRole", 'customer');
     return (
         <div className="flex flex-col text-4xl just-another-hand justify-center items-center min-w-screen min-h-screen -mt-32">
             <h2>Customer Dashboard</h2>
@@ -11,13 +12,19 @@ const CustomerProfile = ({ handleSignOut }) => {
             <ul className="flex flex-col items-center">
                 <li>
                     <button className="bg-[#780000] hover:bg-[#780000] py-2 px-4 rounded-full text-white mt-4"
-                        onClick={() => navigate('/commissions')}>
+                        onClick={() => {
+                            localStorage.setItem('hasReloaded', 'false');
+                            navigate('/commissions');
+                            }}>
                         My Commissions
                     </button>
                 </li>
                 <li>
                     <button className="bg-[#780000] hover:bg-[#780000] py-2 px-4 rounded-full text-white mt-4"
-                        onClick={() => navigate('/contact')}>
+                        onClick={() => {
+                            localStorage.setItem('hasReloaded', 'false');
+                            navigate('/contact')
+                            }}>
                         Customer Support
                     </button>
                 </li>
